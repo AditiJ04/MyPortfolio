288473880b0827f8cb668c9b8685130579772585
aditi <aditijeena1122@gmail.com>
2025-06-26 12:34:51 +0530

    Image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/mylogo.png";
+import mylogo from "../Assets/mylogo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
@@ -39,7 +39,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={mylogo} className="img-fluid logo" alt="AJ" />
+          <img src={mylogo} style={{ width: '80px', height: '80px' }} className="" alt="AJ" />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
